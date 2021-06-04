ecdc2d1602a1d26c4ef72ab1d2a5a19d94678ddf
sarahhope-swaim <85300079+sarahhope-swaim@users.noreply.github.com>
2021-06-04 14:20:04 -0500

    Apply Cadence Design Systems License/Copyright/Disclaimer
    Pointwise is now Cadence Design Systems, Inc.  All files except those explicitly covered by their own license are provided as-is, as per the warranty disclaimer in the README and in all applicable files.

--- rtCaepInitItems.h
+++ rtCaepInitItems.h
@@ -1,10 +1,15 @@
 /****************************************************************************
  *
- * Pointwise Plugin utility functions
+ * (C) 2021 Cadence Design Systems, Inc. All rights reserved worldwide.
  *
- * Proprietary software product of Pointwise, Inc.
- * Copyright (c) 1995-2020 Pointwise, Inc.
- * All rights reserved.
+ * This sample source code is not supported by Cadence Design Systems, Inc.
+ * It is provided freely for demonstration purposes only.
+ * SEE THE WARRANTY DISCLAIMER AT THE BOTTOM OF THIS FILE.
+ *
+ ***************************************************************************/
+/****************************************************************************
+ *
+ * Pointwise Plugin utility functions
  *
  ***************************************************************************/
 
@@ -105,3 +110,14 @@
 /*! \endcond */
 
 #endif /* _RTCAEPINITITEMS_H_ */
+
+/****************************************************************************
+ *
+ * This file is licensed under the Cadence Public License Version 1.0 (the
+ * "License"), a copy of which is found in the included file named "LICENSE",
+ * and is distributed "AS IS." TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE
+ * LAW, CADENCE DISCLAIMS ALL WARRANTIES AND IN NO EVENT SHALL BE LIABLE TO
+ * ANY PARTY FOR ANY DAMAGES ARISING OUT OF OR RELATING TO USE OF THIS FILE.
+ * Please see the License for the full text of applicable terms.
+ *
+ ****************************************************************************/
--- runtimeWrite.cxx
+++ runtimeWrite.cxx
@@ -1,11 +1,16 @@
 /****************************************************************************
  *
+ * (C) 2021 Cadence Design Systems, Inc. All rights reserved worldwide.
+ *
+ * This sample source code is not supported by Cadence Design Systems, Inc.
+ * It is provided freely for demonstration purposes only.
+ * SEE THE WARRANTY DISCLAIMER AT THE BOTTOM OF THIS FILE.
+ *
+ ***************************************************************************/
+/****************************************************************************
+ *
  * CaeUnsADS implementation of runtimeWrite(), runtimeCreate(), and
  * runtimeDestroy()
- *
- * Proprietary software product of Pointwise, Inc.
- * Copyright (c) 1995-2020 Pointwise, Inc.
- * All rights reserved.
  *
  ***************************************************************************/
 
@@ -766,3 +771,14 @@ PWP_VOID
 runtimeDestroy(CAEP_RTITEM * )
 {
 }
+
+/****************************************************************************
+ *
+ * This file is licensed under the Cadence Public License Version 1.0 (the
+ * "License"), a copy of which is found in the included file named "LICENSE",
+ * and is distributed "AS IS." TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE
+ * LAW, CADENCE DISCLAIMS ALL WARRANTIES AND IN NO EVENT SHALL BE LIABLE TO
+ * ANY PARTY FOR ANY DAMAGES ARISING OUT OF OR RELATING TO USE OF THIS FILE.
+ * Please see the License for the full text of applicable terms.
+ *
+ ****************************************************************************/
